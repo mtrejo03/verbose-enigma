f5769597490c90287863644481acfe5671589b4e
Trejo <mtrejo1@una.edu>
2025-01-28 16:41:33 -0600

    new new code

--- app.js
+++ app.js
@@ -1,13 +1,46 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3000;  
 const bodyParser = require('body-parser')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGO_URI;
+
+console.log(uri);
 
 
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'))
+
+
+//begin all my middlewarres
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', function (req, res) {
   res.sendFile('index.html');
@@ -36,9 +69,16 @@ app.post('/saveMyName', (req,res)=>{
 app.get('/saveMyNameGet', (req,res)=>{
   console.log('did we hit the get endpoint?'); 
 
-  console.log(req.query); 
+  console.log('req.query: ', req.query); 
 
-  res.redirect('/ejs'); 
+  //console.log('req.params: ', req.params);
+
+ // res.redirect('/ejs'); 
+
+ let reqName = req.query.myNameGet;
+
+ res.render('word',
+ {pageTitle: reqName});
 
 })
 

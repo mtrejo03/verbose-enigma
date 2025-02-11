e9ffd29945be794aea00ea4a8387d7279b146392
Trejo <mtrejo1@una.edu>
2025-02-11 16:58:18 -0600

    more code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config()
-const shajs = require('sha.js')
+//const shajs = require('sha.js')
 const app = express()
 const port = process.env.PORT || 3000;  
 const bodyParser = require('body-parser')
@@ -8,7 +8,7 @@ const { ObjectId } = require('mongodb')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = process.env.MONGO_URI;
 
-// console.log(uri);
+
 
 
 
@@ -28,154 +28,83 @@ const client = new MongoClient(uri, {
   }
 });
 
-console.log(shajs('sha256').update('cat').digest('hex')); 
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-//run().catch(console.dir);
+const mongoCollection = client.db("millySobieProfile").collection
+("millySobieBlog");
 
+function initProfileData() {
 
-
-async function getData() {
-
-  await client.connect(); 
-  let collection = await client.db("guitar-app-database").collection("guitar-app-songs"); 
- 
-  let results = await collection.find({}).toArray(); 
-    
-  console.log(results); 
-  return results; 
-
+  mongoCollection.insertOne(
+   { name: "me",
+    github: "https://gh.com...",
+    url: "", 
+    status: "looking for job"
+  }  
+);
 }
 
-app.get('/read', async function (req, res) {
-  let getDataResults = await getData(); 
-  console.log(getDataResults); 
-  res.render('songs', 
-    { songData : getDataResults} ); 
+initProfileData(); 
+  // let myData = 
+  // { name: "me",
+  //   github: "https://gh.com...",
+  //   url: "", 
+  //   status: "looking for job"
+  // }  
+
+
+
+
+
+app.get('/', async function (req, res) {
+  
+  let results = await mongoCollection.find({}).toArray();
+
+  res.render('profile', 
+    { profileData : results} ); 
 
 })
 
 app.post('/insert', async (req,res)=> {
-  // app.get('/insert', async (req,res)=> {
+ 
   
-    console.log('in /insert');
-    
-    let newSong = req.body.myName; //only for POST, GET is req.params? 
-    // let newSong = req.query.myName;
-    console.log(newSong);
+    let results = await mongoCollection.insertOne({ 
+      title: req.body.title,
+      post: req.body.post
+    });
   
-    //connect to db,
-    await client.connect();
-    //point to the collection 
-    await client
-      .db("guitar-app-database")
-      .collection("guitar-app-songs")
-      .insertOne({ whatthewhatever: newSong});
   
-    res.redirect('/read');
+    res.redirect('/');
   
   }); 
+
+  app.post('/delete', async function (req, res) {
+  
+    let result = await mongoCollection.findOneAndDelete( 
+    {
+      "_id": new ObjectId(req.body.deleteId)
+    }
+  ).then(result => {
+    
+    res.redirect('/');
+  })
+
+}); 
   
 app.post('/update', async (req,res)=>{
 
-  console.log("req.body: ", req.body)
-
-  client.connect; 
-  const collection = client.db("guitar-app-database")
-  .collection("guitar-app-songs");
-  let result = await collection.findOneAndUpdate( 
-  {"_id": new ObjectId(req.body.nameID)}, { $set: {"fname": req.body.inputUpdateName } }
-)
-.then(result => {
-  console.log(result); 
-  res.redirect('/read');
-})
-}); 
-
-
-app.post('/delete/:id', async (req,res)=>{
-
-  console.log("in delete, req.parms.id: ", req.params.id)
-
-  client.connect; 
-  const collection = client.db("guitar-app-database")
-  .collection("guitar-app-songs");
-  let result = await collection.findOneAndDelete( 
-    {
-      "_id": new ObjectId(req.params.id)
-    }
-  ).then(result => {
-  console.log(result); 
-  res.redirect('/read');})
-
-})
-
-//begin all middlewares
-
-
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
-
-})
-
-
-app.post('/saveMyName', (req,res)=>{
-  console.log('did we hit the post endpoint?'); 
-
-  console.log(req.body); 
-
-  //res.redirect('/ejs'); 
-
-  res.render('word',
-    {pageTitle: req.body.myName});
-
-})
-
-app.get('/saveMyNameGet', (req,res)=>{
-  console.log('did we hit the get endpoint?'); 
-
-  console.log('req.query: ', req.query); 
-
-  //console.log('req.params: ', req.params);
-
-
-
- let reqName = req.query.myNameGet;
-  // res.redirect('/ejs'); 
-
- res.render('word',
- {pageTitle: reqName});
-
-})
-
-app.get('/ejs', function (req, res) {
-  res.render('word',
-    {pageTitle: 'my cool ejs page'}
-  );
-})
-
-
-app.get('/nodemon', function (req, res) {
-  res.send('look ma, no kill node process then restart node then refresh browser...cool?');
-
-})
-
-
-//endpoint, middleware(s)
-app.get('/helloRender', function (req, res) {
-  res.send('Hello Express from Real World<br><a href="/">back to home</a>')
-})
+  let result = await mongoCollection.findOneAndUpdate( 
+    {_id: ObjectId.createFromHexString(req.body.updateId)}, { 
+      $set: 
+        {
+          title : req.body.updateTitle, 
+          post : req.body.updatePost 
+        }
+       }
+    ).then(result => {
+    console.log(result); 
+    res.redirect('/');
+  })
+  }); 
 
 
 

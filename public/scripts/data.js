4b68f55857f1ca4f9da1e0730602250b74cd4e20
mtrejo03 <millydtrejo@gmail.com>
2025-02-27 22:58:50 -0600

    code updates

---
+++ public/scripts/data.js
@@ -0,0 +1,34 @@
+data = {
+    clubs: [
+        {
+            name: "Girls Who Code",
+            major: "CS, IT, CIS",
+            socialURL:["https://www.instagram.com/gwc_una/"]
+        },
+        {
+            name: "Collegiate Entrepreneurs Organization",
+            major: "Innovation and Entrepreneurship",
+            socialURL:["https://www.instagram.com/una_ceo/"]
+        },
+        {
+            name: "Accounting Scholars",
+            major: "Accounting & Business",
+            socialURL:["https://www.instagram.com/unaaccounting/"]
+        },
+        {
+            name: "Women in Business",
+            major: "Business",
+            socialURL:["https://www.instagram.com/una_womeninbusiness/?hl=en"]
+        },
+        {
+            name: "Psychology Club",
+            major: "Psychology",
+            socialURL:["https://www.instagram.com/unapsychclub/"]
+        },
+        {
+            name: "French Club",
+            major: "French",
+            socialURL:["https://www.instagram.com/unafrenchclub/?hl=en"]
+        },
+    ]
+}
